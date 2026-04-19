9251f06fd2976806c02b25025a823e95fac65781
Sagar Saha <Sagarsaha2579@gmail.com>
2026-04-19 14:33:34 -0400

    JS_Variables

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Sagar"; //constant stays locked
+let lastName = "Saha";
+var age = 27; 
 
 // Exercise 2
+lastName = "Saha";
+age = 27;
 
 // Exercise 3
+let language  = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true; 
+
+console.log(`${language} was released on ${createdYear}! `);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false; 
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"; 
+
+console.log(`This item is $${price}. On sale: ${isOnSale}. Discount: ${salePercentage}%. Stock: ${stock}. Selected size: ${selectedSize}.`);
+
+//Exercise 5 
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772; 
+let bookmark = 456;
+let hasRead = true; 
+
+console.log(`You are reading "${title}" by ${author}. You are on page ${bookmark} out of ${pageCount}.`);
